45cd6685fdf199d5b09bc7a13be25eef5421ff6a
Reanipush <reanipush@icloud.com>
2021-06-01 15:42:51 +0300

    Sravnenie

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 "use strict"
 
+const num = 50;
+
+switch(num) {
+    case 49:
+        console.log('Wrong!!!');
+        break;
+    case 48:
+        console.log('Wrong!!!');
+        break;
+        case 50:
+            console.log('Ok!!!');
+            break;   
+            default:
+                console.log('Not in this time');
+                break;
+}
