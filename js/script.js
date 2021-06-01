2e2139478daacb113fd9379edf293ad151485dc1
Reanipush <reanipush@icloud.com>
2021-06-01 16:15:37 +0300

    sravnenie1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 "use strict"
 
+if(4==5){
+    console.log('Super!!');
+} else {console.log('Its bad!')}
+
+
+
+(65 != 77) ? console.log('Super') : console.log('Bad');
+
 const num = 50;
 
 switch(num) {
@@ -9,7 +17,7 @@ switch(num) {
     case 48:
         console.log('Wrong!!!');
         break;
-        case 50:
+        case 58:
             console.log('Ok!!!');
             break;   
             default:
